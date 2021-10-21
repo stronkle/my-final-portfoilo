58842dfaec5f21c1abe52d64ce99aff034c991a0
stronkle <74915477+stronkle@users.noreply.github.com>
2021-10-21 23:36:35 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -5,7 +5,48 @@ menu.onclick = () => {
   menu.classList.toggle("fa-times");
   header.classList.toggle("active");
 };
+//form validiation
+function validate() {
+  var name = document.getElementById("name").value;
+  var subject = document.getElementById("subject").value;
+  var phone = document.getElementById("number").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var error_message = document.getElementById("error_message");
 
+  error_message.style.padding = "10px";
+
+  var text;
+  if (name.length < 5) {
+    text = "Please Enter valid Name";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (subject.length < 10) {
+    text = "Please Enter Correct Subject";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (email.indexOf("@") == -1 || email.length < 6) {
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (isNaN(phone) || phone.length != 11) {
+    text = "Please Enter valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if (message.length <= 100) {
+    text = "Please Enter More Than 100 Characters";
+    error_message.innerHTML = text;
+    return false;
+  }
+  alert("Form Submitted Successfully!");
+  return true;
+}
+//on scroll animation
 window.onscroll = () => {
   menu.classList.remove("fa-times");
   header.classList.remove("active");
